5b2958d5f5981d19892023bf9d63c089a99abd84
Marquardt Snell <thefabulouscube@tadaatiedye.com>
2023-10-01 21:50:45 -0400

    Section 2 Lesson 20 Persistance Service

--- src/app/auth/store/effects.ts
+++ src/app/auth/store/effects.ts
@@ -1,23 +1,33 @@
 import {inject} from '@angular/core'
-import {createEffect, Actions, ofType} from '@ngrx/effects'
-import {catchError, map, of, switchMap} from 'rxjs'
+import {Router} from '@angular/router'
+import {Actions, createEffect, ofType} from '@ngrx/effects'
+import {catchError, map, of, switchMap, tap} from 'rxjs'
+import {PersistanceService} from 'src/app/shared/services/persistance.service'
 import {CurrentUserInterface} from 'src/app/shared/types/currentUser.interface'
 import {AuthService} from '../services/auth.service'
 import {authActions} from './actions'
 import {HttpErrorResponse} from '@angular/common/http'
 
 export const registerEffect = createEffect(
-  (actions$ = inject(Actions), authService = inject(AuthService)) => {
+  (
+    actions$ = inject(Actions),
+    authService = inject(AuthService),
+    persistanceService = inject(PersistanceService)
+  ) => {
     return actions$.pipe(
       ofType(authActions.register),
       switchMap(({request}) => {
         return authService.register(request).pipe(
           map((currentUser: CurrentUserInterface) => {
+            persistanceService.set('accessToken', currentUser.token)
             return authActions.registerSuccess({currentUser})
           }),
           catchError((errorResponse: HttpErrorResponse) => {
+            console.log('Store effects: ', errorResponse.error.error)
             return of(
-              authActions.registerFailure({errors: errorResponse.error.error})
+              authActions.registerFailure({
+                errors: errorResponse.error.error,
+              })
             )
           })
         )
@@ -25,4 +35,16 @@ export const registerEffect = createEffect(
     )
   },
   {functional: true}
+)
+
+export const redirectAfterRegisterEffect = createEffect(
+  (actions$ = inject(Actions), router = inject(Router)) => {
+    return actions$.pipe(
+      ofType(authActions.registerSuccess),
+      tap(() => {
+        router.navigateByUrl('/')
+      })
+    )
+  },
+  {functional: true, dispatch: false}
 )
